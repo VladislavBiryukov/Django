ffbfff8e433aa76cdf213b860fe599b697f191da
Vlad <b.vlad87@mail.ru>
2022-02-08 17:53:29 +0300

    lesson_11. Ordersapp

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -1,8 +1,9 @@
-import mainapp.views as mainapp
 from django.conf import settings
 from django.conf.urls import include
 from django.conf.urls.static import static
 from django.urls import path, re_path
+
+import mainapp.views as mainapp
 
 urlpatterns = [
     re_path(r"^$", mainapp.main, name="main"),
@@ -11,6 +12,7 @@ urlpatterns = [
     path("", include("social_django.urls", namespace="social")),
     re_path(r"^auth/", include("authnapp.urls", namespace="auth")),
     re_path(r"^basket/", include("basketapp.urls", namespace="basket")),
+    re_path(r"^order/", include("ordersapp.urls", namespace="order")),
     re_path(r"^admin/", include("adminapp.urls", namespace="admin")),
 ]
 
