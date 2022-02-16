eb42d47d2caa07d2142583c5838bcaa29e79543f
Vlad <b.vlad87@mail.ru>
2022-02-16 20:46:47 +0300

    lesson_14. debug-tool-bar

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -1,8 +1,9 @@
-import mainapp.views as mainapp
 from django.conf import settings
 from django.conf.urls import include
 from django.conf.urls.static import static
 from django.urls import path, re_path
+
+import mainapp.views as mainapp
 
 urlpatterns = [
     re_path(r"^$", mainapp.main, name="main"),
@@ -17,4 +18,7 @@ urlpatterns = [
 
 
 if settings.DEBUG:
+    import debug_toolbar
+
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += [re_path(r"^__debug__/", include(debug_toolbar.urls))]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,6 +1,5 @@
 import random
 
-from basketapp.models import Basket
 from django.conf import settings
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import get_object_or_404, render
@@ -16,30 +15,29 @@ def main(request):
     return render(request, "mainapp/index.html", content)
 
 
-def get_basket(user):
-    if user.is_authenticated:
-        return Basket.objects.filter(user=user)
-    else:
-        return []
+# def get_hot_product():
+#     products = Product.objects.filter(is_active=True, category__is_active=True)
+#     return random.sample(list(products), 1)[0]
 
 
-def get_hot_product():
-    products = Product.objects.filter(is_active=True, category__is_active=True)
-    return random.sample(list(products), 1)[0]
+# def get_same_products(hot_product):
+#     same_products = Product.objects.filter(category=hot_product.category, is_active=True).exclude(pk=hot_product.pk)[:3]
+#     return same_products
 
 
-def get_same_products(hot_product):
-    same_products = Product.objects.filter(category=hot_product.category, is_active=True).exclude(pk=hot_product.pk)[:3]
-    return same_products
+def get_hot_product_list():
+    products = Product.objects.filter(is_active=True, category__is_active=True).select_related("category")
+    hot_product = random.sample(list(products), 1)[0]
+    hot_list = products.exclude(pk=hot_product.pk)[:3]
+    return (hot_product, hot_list)
 
 
 def products(request, pk=None, page=1):
     title = "продукты"
     links_menu = ProductCategory.objects.filter(is_active=True)
-    basket = get_basket(request.user)
 
     if pk is not None:
-        if pk == "0":
+        if str(pk) == str(0):
             category = {"pk": 0, "name": "все"}
             products = Product.objects.filter(is_active=True, category__is_active=True).order_by("price")
         else:
@@ -62,17 +60,16 @@ def products(request, pk=None, page=1):
             "category": category,
             "products": products_paginator,
             "media_url": settings.MEDIA_URL,
-            "basket": basket,
         }
         return render(request, "mainapp/products_list.html", content)
-    hot_product = get_hot_product()
-    same_products = get_same_products(hot_product)
+    # hot_product = get_hot_product()
+    # same_products = get_same_products(hot_product)
+    hot_product, same_products = get_hot_product_list()
     content = {
         "title": title,
         "links_menu": links_menu,
         "same_products": same_products,
         "media_url": settings.MEDIA_URL,
-        "basket": basket,
         "hot_product": hot_product,
     }
     return render(request, "mainapp/products.html", content)
@@ -84,7 +81,6 @@ def product(request, pk):
         "title": title,
         "links_menu": ProductCategory.objects.filter(is_active=True),
         "product": get_object_or_404(Product, pk=pk),
-        "basket": get_basket(request.user),
         "media_url": settings.MEDIA_URL,
     }
     return render(request, "mainapp/product.html", content)
